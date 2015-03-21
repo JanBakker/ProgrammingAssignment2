7a2be4fab1a6d31ccd7364e89fd7d77154aa5c8f
Jan Bakker <Jan.Bakker@inholland.nl>
2015-03-21 16:32:52 +0100

    Revert "Initial commit for Programming Assignment 2"
    This reverts commit 27987823fcf81d46a5e2186391addd87c079b879.
    
    Conflicts:
    	README.md
    	cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+  ## Jan Bakker, 21 maart 2015
+  ## makeCacheMatrix is a function to create space en structure
+  ## in the memory of your computer (cache) to store an matrix.
+  ## The memoryposition is defined in the set-funtion
+  ## The structure is a list of 4 vectors (set, get, setmatrix, getmatrix)
+  ## In makeCacheMatrix we do not calculate the inverse matrix.
+  ## Calculating of the inverse matrix is done in cacheSolve 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {
+        m <- NULL
+        ## by using <<- we make a pointer to the memoryposition
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <- function() m
+        ## show memoryposition of the cache
+	  list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix()
+	  ## if we've already inverted the matrix 
+	  ## then lets get the inverted matrix from cache
+        if(!is.null(m)) {
+                message("getting inverted matrix from cache")
+                return(m)
+        }
+        data <- x$get()
+        ## invert the matrix and cache in x$setmatrix(m)
+	  m <- solve(data, ...)
+        x$setmatrix(m)
+        m
 }
